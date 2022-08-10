d2742e80f38fae276ab77e052f88104a8245e4c4
martijn van de ree <mrtjnvanderee@gmail.com>
2022-08-10 14:50:00 +0200

    added chakra ui and changed button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Button as ButtonChakra } from '@chakra-ui/react'
+
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <ButtonChakra>{props.label}</ButtonChakra>;
 };
 
 export default Button;
